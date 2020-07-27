35a9ec43bbdeb96d91f3ccaa56f4eebc327fff27
ancheng <ancheng@uvw.org>
2020-07-27 15:50:32 +0800

    update LeetCode

--- leetCode/6.递归和分治.playground/Contents.swift
+++ leetCode/6.递归和分治.playground/Contents.swift
@@ -35,4 +35,28 @@ class Solution {
     }
 }
 
-
+// https://leetcode-cn.com/problems/majority-element/
+/* 169. 多数元素
+给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ 示例 1:
+ 输入: [3,2,3]
+ 输出: 3
+ 示例 2:
+ 输入: [2,2,1,1,1,2,2]
+ 输出: 2
+ */
+extension Solution {
+    func majorityElement(_ nums: [Int]) -> Int {
+        // 摩尔投票法
+        var num = 0
+        var count = 0
+        for n in nums {
+            if count == 0 {
+                num = n
+            }
+            count = n == num ? (count + 1) : (count - 1)
+        }
+        return num
+    }
+}
