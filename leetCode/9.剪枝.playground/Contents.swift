8d639d306be8af2621935e00d3c2e15b2c810201
ancheng <ancheng@uvw.org>
2020-08-03 17:53:24 +0800

    update

--- leetCode/9.剪枝.playground/Contents.swift
+++ leetCode/9.剪枝.playground/Contents.swift
@@ -67,4 +67,98 @@ extension Solution {
     }
 }
 
-Solution().permutation("aab")
+// https://leetcode-cn.com/problems/n-queens/
+/*
+ 51. N皇后 (困难！)
+ n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ 示例:
+ 输入: 4
+ 输出: [
+  [".Q..",  // 解法 1
+   "...Q",
+   "Q...",
+   "..Q."],
+  ["..Q.",  // 解法 2
+   "Q...",
+   "...Q",
+   ".Q.."]
+ ]
+ 解释: 4 皇后问题存在两个不同的解法。
+ 提示：
+ 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一到七步，可进可退。（引用自 百度百科 - 皇后 ）
+ 意思就是棋盘中，每一行每一列每一斜都只有一个皇后
+ */
+extension Solution {
+    func solveNQueens(_ n: Int) -> [[String]] {
+        if n < 1 { return [] }
+        var res: [[String]] = []
+        var cols = Set<Int>()
+        var pie = Set<Int>()
+        var na = Set<Int>()
+
+        var emptyStr = ""
+        for _ in 0..<n {
+            emptyStr.append(".")
+        }
+
+        func dfs(row: Int, curState: [String]) {
+            if row >= n {
+                res.append(curState)
+                return
+            }
+            for col in 0..<n {
+                if cols.contains(col) || pie.contains(row + col) || na.contains(row - col) {
+                    continue
+                }
+                // 把皇后放在这个位置
+                cols.insert(col)
+                pie.insert(row + col)
+                na.insert(row - col)
+
+                var curRes = emptyStr
+                let start = emptyStr.index(emptyStr.startIndex, offsetBy: col)
+                let end = emptyStr.index(emptyStr.startIndex, offsetBy: col + 1)
+                curRes.replaceSubrange(start..<end, with: "Q")
+
+                dfs(row: row + 1, curState: curState + [curRes])
+
+                cols.remove(col)
+                pie.remove(row + col)
+                na.remove(row - col)
+            }
+        }
+
+        dfs(row: 0, curState: [])
+        return res
+    }
+}
+
+// https://leetcode-cn.com/problems/n-queens-ii/
+/*
+ 52. N皇后 II
+ n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ 上图为 8 皇后问题的一种解法。
+ 给定一个整数 n，返回 n 皇后不同的解决方案的数量。
+ 示例:
+ 输入: 4
+ 输出: 2
+ 解释: 4 皇后问题存在如下两个不同的解法。
+ [
+  [".Q..",  // 解法 1
+   "...Q",
+   "Q...",
+   "..Q."],
+
+  ["..Q.",  // 解法 2
+   "Q...",
+   "...Q",
+   ".Q.."]
+ ]
+ */
+extension Solution {
+    func totalNQueens(_ n: Int) -> Int {
+        return solveNQueens(n).count
+    }
+}
